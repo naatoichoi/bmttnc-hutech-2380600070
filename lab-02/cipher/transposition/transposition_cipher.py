3cff2a88864506fc3082d58893a67ebda43d0709
naatoichoi <phamngkcanh@gmail.com>
2026-03-10 11:42:50 +0000

    [add] lab02

---
+++ lab-02/cipher/transposition/transposition_cipher.py
@@ -0,0 +1,46 @@
+class TranspositionCipher:
+    def __init__(self):
+        pass
+
+    def encrypt(self, text, key):
+        encrypted_text = ''
+        for col in range(key):
+            pointer = col
+            while pointer < len(text):
+                encrypted_text += text[pointer]
+                pointer += key
+        return encrypted_text
+
+    # def decrypt(self, text, key):
+    #     decrypted_text = [''] * key
+    #     row, col = 0, 0
+    #     for symbol in text:
+    #         decrypted_text[col] += symbol
+    #         col += 1
+    #         if col == key or (col == key - 1 and row >= len(text) % key):
+    #             col = 0
+    #             row += 1
+    #     return ''.join(decrypted_text)
+    
+    def decrypt(self, text, key):
+        num_cols = len(text) // key
+        if len(text) % key != 0:
+            num_cols += 1
+
+        num_rows = key
+        num_shaded_boxes = (num_cols * num_rows) - len(text)
+
+        decrypted_text = [''] * num_cols
+
+        row = 0
+        col = 0
+
+        for symbol in text:
+            decrypted_text[col] += symbol
+            col += 1
+
+            if (col == num_cols) or (col == num_cols - 1 and row >= num_rows - num_shaded_boxes):
+                col = 0
+                row += 1
+
+        return ''.join(decrypted_text)
